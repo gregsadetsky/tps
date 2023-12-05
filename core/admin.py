52714e27e0cf802a4f0b6f98060eacf97d0e7dc1
gregsadetsky <lepetitg@gmail.com>
2023-12-05 14:18:49 -0500

    .

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import Caller, Game, User
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Caller)
+admin.site.register(Game)
--- core/models.py
+++ core/models.py
@@ -22,3 +22,6 @@ class Caller(models.Model):
     CALLER_STATE = [("hungup", "hungup"), ("ingame", "ingame")]
     state = models.CharField(max_length=255, choices=CALLER_STATE)
     game = models.ForeignKey(Game, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return self.phone_number
