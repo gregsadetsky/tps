08afeb378fbe496d3b9e6907cef7351eca9878d0
gregsadetsky <lepetitg@gmail.com>
2023-12-13 23:13:49 -0500

    .

--- tps/settings/base.py
+++ tps/settings/base.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "core.middleware.add_call_session_to_request",
 ]
 
