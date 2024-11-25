3fb0019ff2c6a96e36fe47f7865ddaeeab60f7bc
gregsadetsky <lepetitg@gmail.com>
2024-11-24 23:33:54 -0500

    .

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pippip install --force-reinstall -U setuptools
+#pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
